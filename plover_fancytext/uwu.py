28f13534f4e3caeca894fd0e9b75443ceddd5523
Seth Rider <seth@thumbquat.com>
2020-11-19 18:46:16 +0000

    Code style

--- plover_fancytext/uwu.py
+++ plover_fancytext/uwu.py
@@ -17,9 +17,6 @@ class Uwu(FancyBase):
     def __init__(self, intense: bool):
         self.intense = intense
 
-    def reset_state(self):
-        pass
-
     def uwuify(self, string: str) -> str:
         string = re.sub("([nN])([aeiouAEIOU])", r"\1y\2", string)
         string = string \
@@ -30,22 +27,23 @@ class Uwu(FancyBase):
         return ''.join([self.uwuify_char(char) for char in string])
 
     def uwuify_char(self, char: str) -> str:
-        result = substitute_with_map(UWU_SUBSTITUTIONS, char)
+        result = self.substitute_with_map(UWU_SUBSTITUTIONS, char)
         if self.intense:
-            result = substitute_with_map(INTENSE_UWU_SUBSTITUTIONS, result)
+            result = self.substitute_with_map(INTENSE_UWU_SUBSTITUTIONS,
+                                              result)
+
+    def substitute_with_map(self, substitutions, char: str) -> str:
+        result = substitutions.get(char.lower())
+        if result is None:
+            return char
+        elif char.isupper():
+            return result.capitalize()
+        else:
+            return result
+
         return result
 
     def __call__(self, string: str) -> str:
         if string:
             return self.uwuify(string)
         return None
-
-
-def substitute_with_map(substitutions, char: str) -> str:
-    result = substitutions.get(char.lower())
-    if result is None:
-        return char
-    elif char.isupper():
-        return result.capitalize()
-    else:
-        return result
